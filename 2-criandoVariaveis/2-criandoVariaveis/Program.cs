19807cfdbc576b5214f2ab25ebec1570f2ca2fa9
Matheus Henrique de Abreu <matheuslink18@hotmail.com>
2022-12-08 17:41:20 -0300

    Add files via upload

---
+++ 2-criandoVariaveis/2-criandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+        int idade;
+
+        idade = 27;
+
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 27 - 5;
+
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 5 * 2 - 6;
+
+        Console.Write(idade);
+
+        Console.WriteLine("Tecle enter para fechar ... " + idade);
+        Console.ReadLine();
+    }
+}
---
+++ caracteresETexto/Program.cs
@@ -0,0 +1,26 @@
+﻿class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 5 - caracteres e textos");
+
+        char letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Curso de Tech ";
+        Console.WriteLine(primeiraFrase + 2022);
+        // O @ ajuda a fazer a listagem da string.
+        string contando = @"contar ém até: 
+        1 
+        2 
+        3 
+        4";
+        Console.WriteLine(contando);
+
+        Console.WriteLine("Tecle enter para fechar... ");
+        Console.ReadLine();
+    }
+   }
